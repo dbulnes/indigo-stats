fb2e92e4984cb86afaf99edcea888e7074207962
David Bulnes <d@vidbuln.es>
2026-09-20 22:27:22 -0700

    Release 0.2.6

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ from . import db,jobs,config
 from .air import aqi,nowcast,environment_values,environment_sql
 from typing import Literal
 
-APP_VERSION = '0.2.5'
+APP_VERSION = '0.2.6'
 @asynccontextmanager
 async def lifespan(app):
     db.initialize()
