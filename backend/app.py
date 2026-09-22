3c464d61d1f887b5b39aee627409e96a9d6d9642
David Bulnes <d@vidbuln.es>
2026-09-22 00:32:35 -0700

    Release 0.7.0

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ from . import db,jobs,config,backups,astronomy
 from .air import aqi,nowcast,environment_values,environment_sql
 from typing import Literal
 
-APP_VERSION = '0.6.9'
+APP_VERSION = '0.7.0'
 SCHEDULES = (
     ('sensor', jobs.collect, 60),
     ('weather', jobs.weather, 3600),
